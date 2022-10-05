843ab09de1a25d5f34e32eccac3dc5355bbb6f2a
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-10-05 17:30:57 -0500

    removed .env

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
+
+
 const router = new Navigo("/");
 
 function render(state = store.Home) {
