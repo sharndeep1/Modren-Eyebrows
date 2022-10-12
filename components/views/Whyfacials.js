56f3da170c43c2e39375ad733e7591a57762891b
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-10-12 16:52:34 -0500

    made changes in css

--- components/views/Whyfacials.js
+++ components/views/Whyfacials.js
@@ -12,8 +12,10 @@ export default (state) => html`
     Facials help to renew skin cells by circulating the blood flow under the skin, which then can decrease
     puffiness and fluid build up that comes from our lymphatic system. Good circulation is important in that it
      brings oxygen, proteins and nutrition to the skin, while ridding our skin of toxins.
-     It helps your skin stay in good condition: You're shedding skin cells throughout the day, so it's important to keep your
-    skin glowing and in good condition. An effective routine can help prevent acne, treat wrinkles, and help keep your skin looking its best
+     It helps your skin stay in good condition: You're shedding skin cells throughout the day, so it's
+      important to keep your skin glowing and in good condition. An effective routine can help prevent acne,
+      treat wrinkles, and help
+    keep your skin looking its best
     </p>
   </section>
 `;
