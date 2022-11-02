5f18344bf4aeb2fe4daa6cc2467adbd2b1f3dd45
sharndeep1 <sharndeep.chahal@yahoo.in>
2022-11-02 18:30:08 -0500

    fix the text

--- components/views/Home.js
+++ components/views/Home.js
@@ -13,9 +13,9 @@ export default (state) => html`
     <h3 align = center> about us </h3>
     <section>
     <p id="good">
-    we  are focused on providing high quality services  and customer satisfaction. I started my career in the aesthetics industry about eight years ago.
-    we are providing facial,threading  and waxing services.we are using herbal products in facials.
-    Threading has been used  to  remove unwanted hair  since ancient times.
+    We are focused on providing high quality services and customer satisfaction.I started my career in the aesthetics industry about eight years ago.
+    We are providing facial, threading and waxing services.We are using herbal products in facials.
+    Threading has been used to remove unwanted hair since ancient times.
     The method of hair removal is more effective and less painful then tweezing and waxing.
     It is longer lasting and more sanitary then using a razer blade and wax.
     It can be used on any part of the face and body. the process only takes 3-5 mins.
